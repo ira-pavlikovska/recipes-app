a038d5727ad0af73ff5ff3d66dd93b0dfb073fba
Iryna Pavlikovska <irina.pavlikovskaya@gmail.com>
2023-01-26 10:47:49 -0700

    added modal component to RecipesPage.tsx

--- src/containers/RecipesPage.tsx
+++ src/containers/RecipesPage.tsx
@@ -12,15 +12,20 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import {useNavigate} from "react-router-dom";
-import { setRecipes } from "../reducer/recipesReducer";
+import {setRecipes} from "../reducer/recipesReducer";
 import {useAppDispatch} from "../hooks/useAppDispatch";
+import ModalComponent from '../components/Modal';
+import Typography from '@mui/material/Typography';
+
 function RecipesPage() {
+
+    const [openModal, setOpenModal] = React.useState(false);
     let navigate = useNavigate();
     const dispatch = useAppDispatch();
     const {user} = useAppSelector((state: RootState) => state.userReducer);
     const {recipes} = useAppSelector((state: RootState) => state.recipesReducer);
     const userId = user.userId
-    if(!userId){
+    if (!userId) {
         navigate('/')
     }
 
@@ -32,6 +37,13 @@ function RecipesPage() {
 
     console.log(`recipes ${JSON.stringify(recipes)}`)
 
+    const handleAddRecipe = () => {
+        setOpenModal(true);
+    }
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    }
+
     const StyledPaper = styled(Paper)(({theme}) => ({
         backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
         ...theme.typography.body2,
@@ -39,35 +51,43 @@ function RecipesPage() {
         textAlign: 'center',
         color: theme.palette.text.secondary,
     }));
+
+
     const haveRecipes = recipes.length > 0;
 
     return (
         <Box sx={{flexGrow: 1}}>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
-                    <div style={{alignItems: 'center' }} ><Header/></div>
+                    <div style={{alignItems: 'center'}}><Header/></div>
                 </Grid>
                 <Grid item xs={12}>
                     <StyledPaper elevation={3}>
                         <Grid item xs={12} style={{marginTop: 30}}>
-                            <span style={{fontSize: 22, fontWeight: 'bold', float: 'left', marginLeft: 30}}>My Recipes</span>
+                            <span style={{
+                                fontSize: 22,
+                                fontWeight: 'bold',
+                                float: 'left',
+                                marginLeft: 30
+                            }}>My Recipes</span>
                             <span><Button
                                 variant="outlined"
                                 style={{float: 'right'}}
+                                onClick={handleAddRecipe}
                             >
                                 Add new Recipe
                             </Button>
                             </span>
                         </Grid>
                         <Grid item xs={12}>
-                                {
-                                    haveRecipes && (
-                                        recipes.map((recipe:RecipeType) => (
-                                            <div><RecipeComponent recipe={recipe}/></div>
-                                        ))
-                                    )
-                                }
-
+                            {
+                                haveRecipes && (
+                                    recipes.map((recipe: RecipeType) => (
+                                        <div><RecipeComponent recipe={recipe}/></div>
+                                    ))
+                                )
+                            }
+                            <ModalComponent handleCloseModal={handleCloseModal} open={openModal}/>
                         </Grid>
                     </StyledPaper>
                 </Grid>
