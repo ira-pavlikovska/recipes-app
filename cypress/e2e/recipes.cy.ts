8c4d647d1987d066818780d0374257e5f6348f89
Iryna Pavlikovska <irina.pavlikovskaya@gmail.com>
2023-03-10 09:47:54 -0700

    add cypress before each function to log in to recipes page

--- cypress/e2e/recipes.cy.ts
+++ cypress/e2e/recipes.cy.ts
@@ -1,35 +1,41 @@
 describe('recipes page spec', () => {
-    // it('add new recipe flow', () => {
-    //     cy.visit('http://localhost:3000/')
-    //
-    //     cy.get('[data-testid="username-input"]').type('iryna')
-    //     cy.get('[data-testid="password-input"]').type('asdf')
-    //     cy.get('[data-testid="login-button"]').click()
-    //
-    //     cy.url().should('include', '/recipes')
-    //     cy.get('[data-testid="header-element"]').should('contain', "Iryna's Recipes book")
-    //     cy.get('[data-testid="title-element"]').should('contain', "My Recipes")
-    //     cy.get('[data-testid="list-mode-icon"]').click()
-    //     cy.get('[data-testid="add-recipe-button"]').click()
-    //     cy.get('[data-testid="modal-label-element"]').should('contain', "Recipe name")
-    //     cy.get('[data-testid="input[name=recipeName]"]').type("pasta")
-    //     cy.get('[data-testid="input[name=name]"]').type("spaghetti")
-    //     cy.get('[data-testid="input[name=quantity]"]').type("1 pkg")
-    //     cy.get('[data-testid="add-ingredients-button"]').click()
-    //     cy.get('[data-testid="ingredient-name"]').should('contain', "spaghetti")
-    //     cy.get('[data-testid="ingredient-quantity"]').should('contain', "1 pkg")
-    //     cy.get('[data-testid="input[name=step]"]').type("boil the water")
-    //     cy.get('[data-testid="add-instruction-button"]').click()
-    //     cy.get('[data-testid="instruction-step"]').should('contain', "boil the water")
-    //     cy.get('[data-testid="add-edit-button"]').click()
-    //     cy.get('[data-testid="recipe-name"]').should('contain', "pasta")
-    // })
-    it('search input flow', () => {
+
+    beforeEach(() => {
         cy.visit('http://localhost:3000/')
 
         cy.get('[data-testid="username-input"]').type('iryna')
         cy.get('[data-testid="password-input"]').type('asdf')
         cy.get('[data-testid="login-button"]').click()
+    })
+
+    afterEach(() => {
+        // clean up
+        // remove added data ...
+    })
+
+    it('add new recipe flow', () => {
+
+        cy.url().should('include', '/recipes')
+        cy.get('[data-testid="header-element"]').should('contain', "Iryna's Recipes book")
+        cy.get('[data-testid="title-element"]').should('contain', "My Recipes")
+        cy.get('[data-testid="list-mode-icon"]').click()
+        cy.get('[data-testid="add-recipe-button"]').click()
+        cy.get('[data-testid="modal-label-element"]').should('contain', "Recipe name")
+        cy.get('[data-testid="input[name=recipeName]"]').type("pasta")
+        cy.get('[data-testid="input[name=name]"]').type("spaghetti")
+        cy.get('[data-testid="input[name=quantity]"]').type("1 pkg")
+        cy.get('[data-testid="add-ingredients-button"]').click()
+        cy.get('[data-testid="ingredient-name"]').should('contain', "spaghetti")
+        cy.get('[data-testid="ingredient-quantity"]').should('contain', "1 pkg")
+        cy.get('[data-testid="input[name=step]"]').type("boil the water")
+        cy.get('[data-testid="add-instruction-button"]').click()
+        cy.get('[data-testid="instruction-step"]').should('contain', "boil the water")
+        cy.get('[data-testid="add-edit-button"]').click()
+        cy.get('[data-testid="recipe-name"]').should('contain', "pasta")
+    })
+
+    it('search input flow', () => {
+
         cy.get('[data-testid="search-input"]').type("shrimp")
         cy.get('[data-testid="list-mode-icon"]').click()
         cy.get('[data-testid="list-recipes"]').then($elements => {
